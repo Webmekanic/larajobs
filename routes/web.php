30af479b66104d935bad50b4ecb273e4724f9ecd
David Aniebo <aniebovictor001@gmail.com>
2023-01-18 12:48:29 +0100

    Feat:Routing in Laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return  response('Hello World' , 400)
+    ->header('Content-Type', 'text/plain')
+    ->header('foo', 'bar'); 
+});
+
+Route::get('/posts/{id}', function ($id) {
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
