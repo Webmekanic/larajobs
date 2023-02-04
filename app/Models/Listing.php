f155132f0567b95a76ca2f97303d7f3b920c2246
Webmekanic <aniebovictor001@gmail.com>
2023-02-04 13:42:32 +0100

    Feat:Create listing model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,29 +1,11 @@
-<?php 
+<?php
+
 namespace App\Models;
 
- class Listing {
-    public static function all(){
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing one',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adip'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing two',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adip'
-            ],
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id){
-        $listings = self::all();
-
-        foreach($listings as $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
- };
+class Listing extends Model
+{
+    use HasFactory;
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Listing::create([
+            'title' => 'Laravel Senior Developer',
+            'tags' => 'Laravel, JavaScript',
+            'company' => 'Acme Corp',
+            'Location' => 'Boston, MA',
+            'email' => 'test@example.com',
+            'website' => 'http://example.com',
+            'description' => 'lorem ipsum ahhhrdb sdusuhdusd sduhsudh rmlhgfsigqrcec',
+        ]);
+
+        Listing::create([
+            'title' => 'Full-Stack Engineer',
+            'tags' => 'laravel, backend, api',
+            'company' => 'Stark Industries',
+            'location' => 'New York, NY',
+            'email' => 'email2@email.com',
+            'website' => 'http://example.com',
+            'description' => 'lorem ipsum ahhhrdb sdusuhdusd sduhsudh rmlhgfsigqrcec',
+        ]);
     }
 }
