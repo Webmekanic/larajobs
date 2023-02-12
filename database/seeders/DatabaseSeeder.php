381035972eabe107b4d96a27ce23edb04a531de1
Webmekanic <aniebovictor001@gmail.com>
2023-02-12 12:49:10 +0100

    Feat:Create factory for listing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,30 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory(5)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        Listing::create([
-            'title' => 'Laravel Senior Developer',
-            'tags' => 'Laravel, JavaScript',
-            'company' => 'Acme Corp',
-            'Location' => 'Boston, MA',
-            'email' => 'test@example.com',
-            'website' => 'http://example.com',
-            'description' => 'lorem ipsum ahhhrdb sdusuhdusd sduhsudh rmlhgfsigqrcec',
-        ]);
-
-        Listing::create([
-            'title' => 'Full-Stack Engineer',
-            'tags' => 'laravel, backend, api',
-            'company' => 'Stark Industries',
-            'location' => 'New York, NY',
-            'email' => 'email2@email.com',
-            'website' => 'http://example.com',
-            'description' => 'lorem ipsum ahhhrdb sdusuhdusd sduhsudh rmlhgfsigqrcec',
-        ]);
+        Listing::factory(6)->create();
     }
 }
